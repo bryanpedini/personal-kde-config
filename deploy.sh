8866dc5beca8c7a65864e93f79aa7abb84c5dea1
Bryan Joshua Pedini <b.pedini@bjphoster.com>
2021-06-03 17:39:45 +0200

    quality of life, terminal server version - cleanup is now opt-in instead of opt-out - added server parameter to deploy with a different terminal scheme

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 THIS="$(dirname "$(readlink -fm "$0")")"
-CLEANUP=true
+SERVER=FALSE
+CLEANUP=false
 
 _arguments() {
   for PARM in "$@"; do
-    if [ "${PARM}" = "--no-cleanup" ]; then
-      CLEANUP=false
+    if [ "${PARM}" = "--server" ]; then
+      SERVER=TRUE
+    elif [ "${PARM}" = "--cleanup" ]; then
+      CLEANUP=TRUE
     elif [ "${PARM}" = "-h" ] || [ "${PARM}" = "--help" ]; then
       _help
       exit 0
@@ -15,11 +18,12 @@ _arguments() {
 }
 
 _help() {
-  echo "Usage: $(readlink -fm "$0") [-h | --help] [--no-cleanup]"
+  echo "Usage: $(readlink -fm "$0") [-h | --help] [--cleanup]"
   echo
   echo "Options:"
   echo "  -h | --help : Prints this help message and quits"
-  echo "  --no-cleanup: Does not clean the source folder after adding the bash overrides"
+  echo "  --server: Customizes the terminal feel for a server installation"
+  echo "  --cleanup: Removes the source folder after installation"
   echo
 }
 
@@ -46,6 +50,10 @@ _main() {
     _bash_overrides
     echo "bash overrides added"
     echo
+  fi
+
+  if [ "${SERVER}" = true ]; then
+    sed -i 's/terminal_fancyfying/terminal_fancyfying_server/' ~/.bashrc_overrides/_all
   fi
 
   if [ "${CLEANUP}" = true ]; then
